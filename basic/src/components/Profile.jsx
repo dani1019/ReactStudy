7772c0be02352a2fec1bdf60ba6235c59230e3df
jooyong yang <nihonkita17@gmail.com>
2024-02-20 07:49:30 +0900

    profile

--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -1,11 +1,12 @@
-export default function Profile(){
+export default function Profile({image, name, title, newBool}){
     return <div className='profile'>
         <img
             className='photo' 
-            src=" https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+            src = {image}
             alt="avater"
         />
-        <h1>James Kim</h1>
-        <h1>frontEngineer</h1>
+        {newBool=== "new"? <div class = "new" style="display:block"/>: <div class = "new" style="display:none"/>}
+        <h1>{name}</h1>
+        <h1>{title}</h1>
     </div>
 }
